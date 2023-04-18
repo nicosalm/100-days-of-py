38fdc97358d0a5edce3405be2bf7480a2aef44a0
Nico Salm <nico@salm.dev>
2023-04-18 00:39:00 -0500

    day3 commenting

--- day3_dragontreasuregame/treasure.py
+++ day3_dragontreasuregame/treasure.py
@@ -1,7 +1,9 @@
 from typing import Dict, List
 from random import Random
 
+# the Room class represents a room in the game
 class Room:
+    # each room has a name, description, list of items, and dictionary of exits
     name: str
     description: str
     items: list
@@ -28,21 +30,25 @@ class Room:
     def get_items(self):
         return self.items
 
+# the Player class represents the player in the game, and has a name, room, and inventory which is a list of items
 class Player:
     def __init__(self, name: str, room: Room):
         self.name = name
         self.room = room
         self.inventory = []
     
+    # the move method moves the player to a new room
     def move(self, new_room: Room):
         self.room = new_room
-        
+    
+    # the get_item method adds an item to the player's inventory
     def get_item(self, item: str):
         if item in self.room.get_items() and item not in self.inventory:
            self.room.remove_item(item)
            self.inventory.append(item)
         print(f"You picked up {item}.")
    
+# the TreasureRoom class is a subclass of the Room class, and has an additional method to unlock the chest
 class TreasureRoom(Room):
     def __init__(self, name: str, description: str, items: list, exits: dict):
         super().__init__(name, description, items, exits)
@@ -58,25 +64,36 @@ class TreasureRoom(Room):
             print("You open the chest and find a treasure!")
         else:
             print("The chest is locked. You need a key to open it.")
-
+            
+# TODO: implement a portal room which is a special room which teleports the player to a random room in the game
+# class PortalRoom(Room): 
+            
+# the Dragon class represents the dragon in the game, and has a name, room, and a method to attack the player. 
+# TODO: dragon moves around randomly, and attacks player if they are in the same room. Sword breaks after one use.
 class Dragon:
     def __init__(self, name: str, room: Room):
         self.name = name
         self.room = room
-        
+
+    # the move method moves the dragon to a new room
     def move(self, new_room: Room):
         self.room = new_room
         
+    # the attack method attacks the player if they are in the same room as the dragon
     def attack(self, player: Player):
+        # if the player doesn't have a sword, they lose the game
         if player.room == self.room and "sword" not in player.inventory:
             print("The dragon attacks you! You lose!")
             game_over = {"game_over": True, "win": False}
             return True
         else:
+            # if the player has a sword, they fend off the dragon and the sword breaks
             if player.room == self.room and "sword" in player.inventory:
                 print("The dragon attacks you, but you fend it off with your sword! Your sword breaks.")
             return False
         
+# the main game loop, which creates the rooms, player, and dragon, and then runs the game.
+# TODO: randomize the rooms, items, and exits in each room, create procedural replayability
 class Game:
     random = Random()
     rooms: List[Room]
@@ -92,6 +109,7 @@ class Game:
         self.game_over = False
         self.win = False
     
+    # game loop
     def play(self):
         while not self.game_over:
             print(self.player.room)
@@ -129,7 +147,7 @@ class Game:
             if room.name == name:
                 return room
             
-# main program
+# main function which gives our particular game a name, creates the rooms, player, and dragon, and then runs the game
 if __name__ == "__main__":
     # create rooms
     room1 = Room("Room 1", "You are in a dark room.", [], {"east": "Room 4", "south": "Room 2", "north": "Room 5"})

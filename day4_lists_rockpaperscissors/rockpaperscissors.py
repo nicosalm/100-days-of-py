ad77d9f24bcac3d8631d0807ee101178f6060702
Nico Salm <nico@salm.dev>
2023-04-20 10:54:27 -0500

    simplified day 4

--- day4_lists_rockpaperscissors/rockpaperscissors.py
+++ day4_lists_rockpaperscissors/rockpaperscissors.py
@@ -1,46 +1,44 @@
 # rock paper scissors game
 import random
 
-def main():
-    print("Welcome to Rock, Paper, Scissors!")
-    print("The rules are simple: rock beats scissors, scissors beats paper, and paper beats rock. Best of 3!")
+# print the rules of the game
+print("Welcome to Rock, Paper, Scissors!")
+print("The rules are simple: rock beats scissors, scissors beats paper, and paper beats rock. Best of 3!")
     
-    # create a dictionary of the possible choices
-    choices = {"rock": 0, "paper": 1, "scissors": 2}
+# create a dictionary of the possible choices
+choices = {"rock": 0, "paper": 1, "scissors": 2}
     
-    # create a dictionary of the possible outcomes
-    outcomes = {"win": 0, "lose": 1, "tie": 2}
+# create a dictionary of the possible outcomes
+outcomes = {"win": 0, "lose": 1, "tie": 2}
     
-    user_strikes = 0
-    computer_strikes = 0
+user_strikes = 0
+computer_strikes = 0
     
-    # now, let's play the game, best of 3
-    while user_strikes < 2 and computer_strikes < 2:
-        user_choice = input("Please enter your choice (rock, paper, or scissors): ")
-        computer_choice = random.choice(list(choices.keys()))
+# now, let's play the game, best of 3
+while user_strikes < 2 and computer_strikes < 2:
+    user_choice = input("Please enter your choice (rock, paper, or scissors): ")
+    computer_choice = random.choice(list(choices.keys()))
         
-        # determine the outcome
-        outcome = (choices[user_choice] - choices[computer_choice]) % 3
-        if outcome == outcomes["win"]:
-            print("You win!")
-            computer_strikes += 1
+    # determine the outcome
+    outcome = (choices[user_choice] - choices[computer_choice]) % 3
+    if outcome == outcomes["win"]:
+        print("You win!")
+        computer_strikes += 1
             
-        elif outcome == outcomes["lose"]:
-            print("You lose!")
-            user_strikes += 1
+    elif outcome == outcomes["lose"]:
+        print("You lose!")
+        user_strikes += 1
             
-        else:
-            print("It's a tie!")
-            continue
-        
-        print("Your score: ", user_strikes)
-        print("Computer score: ", computer_strikes)
-        
-    if user_strikes == 2:
-        print("You win the game!")
     else:
-        print("You lose the game!")
-        
-if __name__ == "__main__":
-    main()
+        print("It's a tie!")
+        continue
     
+    # print scores after each round
+    print("Your score: ", user_strikes)
+    print("Computer score: ", computer_strikes)
+
+# print the final outcome after the game is over
+if user_strikes == 2:
+    print("You win the game!")
+else:
+    print("You lose the game!")

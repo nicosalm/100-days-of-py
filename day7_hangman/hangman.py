c27f251103ff9481da2dee3f00f61aa5c2f55dc9
Nico <nico@varrix.dev>
2023-04-21 10:12:43 -0500

    update to hangman

--- day7_hangman/hangman.py
+++ day7_hangman/hangman.py
@@ -1,81 +1,47 @@
-#importing the time module
+# importing 
 import time
 import random
 from english_words import get_english_words_set # https://pypi.org/project/english-words/
 
-#welcoming the user
+
 name = input("What is your name? ")
+print ("Hello, " + name, "– Time to play hangman!")
 
-print ("Hello, " + name, " – Time to play hangman!")
-
-#wait for 1 second
-time.sleep(1)
+time.sleep(1) # 1 second delay
 
 print ("Start guessing...")
 time.sleep(0.5)
 
-# here we set the secret. You can select any word to play with. 
+# here we set the secret word.
 web2lowerset = get_english_words_set(['web2'], lower=True)
 word = random.choice(list(web2lowerset))
 
-#creates an variable with an empty value
-guesses = ''
 
-#determine the number of turns
+guesses = ''
 turns = 10
 
-# Create a while loop
-
-#check if the turns are more than zero
+# while the turns are more than zero
 while turns > 0:         
-
-    # make a counter that starts with zero
-    failed = 0             
-
-    # for every character in secret_word    
+    
+    failed = 0   
+              
     for char in word:      
-
-    # see if the character is in the players guess
         if char in guesses:    
-    
-        # print then out the character
             print (char,end=""),    
-
         else:
-    
-        # if not found, print a dash
             print ("_",end=""),     
-       
-        # and increase the failed counter with one
             failed += 1    
 
-    # if failed is equal to zero
-
-    # print You Won
     if failed == 0:        
         print (" ... You won")
-    # exit the script
         break            
-    # ask the user go guess a character
+    
     guess = input(" ... guess a character: ") 
-
-    # set the players guess to guesses
     guesses += guess                    
 
-    # if the guess is not found in the secret word
     if guess not in word:  
- 
-     # turns counter decreases with 1 (now 9)
-        turns -= 1        
- 
-    # print wrong
+        turns -= 1
         print ("Wrong")  
- 
-    # how many turns are left
         print ("You have", + turns, 'more guesses' )
- 
-    # if the turns are equal to zero
         if turns == 0:           
-    
-        # print "You Lose"
             print ("You Lose"  )

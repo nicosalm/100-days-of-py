e8c06de04e331abc29e336806b07c499328ee91a
Nico <nico@varrix.dev>
2023-04-23 16:44:00 -0500

    day 8 update

--- day8_caesar-cipher/cipher.py
+++ day8_caesar-cipher/cipher.py
@@ -1,34 +1,31 @@
 # code a cipher that takes a string and a key and a mode and returns a string
 
-def cipher(str: str, key: int, mode: str) -> str:
-    """Encrypts or decrypts a string using a key and a mode"""
-    if mode == "encrypt":
-        return encrypt(str, key)
-    elif mode == "decrypt":
-        return decrypt(str, key)
-    else:
-        return "Invalid mode"
-    
-def encrypt(str: str, key: int) -> str:
+def cipher(mode: str, string: str, key: int) -> str:
     """Encrypts a string using a key"""
-    encrypted = ""
-    for char in str:
-        encrypted += chr(ord(char) + key)
-    return encrypted
-
-def decrypt(str: str, key: int) -> str:
-    """Decrypts a string using a key"""
-    decrypted = ""
-    for char in str:
-        decrypted += chr(ord(char) - key)
-    return decrypted
+    new_str = ""
+    for char in string:
+        if mode == "encrypt":
+            if char.isupper():
+                new_str += chr((ord(char) + key - 65) % 26 + 65)
+            elif char.islower():
+                new_str += chr((ord(char) + key - 97) % 26 + 97)
+            else:
+                new_str += char
+        elif mode == "decrypt":
+            if char.isupper():
+                new_str += chr((ord(char) - key - 65) % 26 + 65)
+            elif char.islower():
+                new_str += chr((ord(char) - key - 97) % 26 + 97)
+            else:
+                new_str += char
+    return new_str
 
 def main():
     """Main function"""
     mode = input("Enter a mode (encrypt, decrypt): ")
-    str = input("Enter a string: ")
+    string = input("Enter a string: ")
     key = int(input("Enter a key: "))
-    print(cipher(str, key, mode))
+    print(cipher(mode, string, key))
     
 if __name__ == "__main__":
     main()

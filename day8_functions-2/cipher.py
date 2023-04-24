45fc868e4b4dd5468a444b056623d353b95e1736
Nico Salm <nico@salm.dev>
2023-04-24 18:45:25 -0500

    formatting updates

--- day8_functions-2/cipher.py
+++ day8_functions-2/cipher.py
@@ -2,16 +2,18 @@
     This program encrypts and decrypts a string using a key, shifting each letter by the key.
 """
 def cipher(mode: str, string: str, key: int) -> str:
-    """Encrypts a string using a key"""
+    # encrpyts or decrypts a string using a key
     new_str = ""
     for char in string:
+
         if mode == "encrypt":
-            if char.isupper():
-                new_str += chr((ord(char) + key - 65) % 26 + 65)
-            elif char.islower():
-                new_str += chr((ord(char) + key - 97) % 26 + 97)
-            else:
-                new_str += char
+            if char.isupper(): # if char is uppercase
+                new_str += chr((ord(char) + key - 65) % 26 + 65) # we shift the char by the key (in ASCII) and wrap around the alphabet
+            elif char.islower(): # if char is lowercase
+                new_str += chr((ord(char) + key - 97) % 26 + 97) 
+            else: # if char is not a letter
+                new_str += char # this is to keep the spaces and punctuation
+
         elif mode == "decrypt":
             if char.isupper():
                 new_str += chr((ord(char) - key - 65) % 26 + 65)
@@ -19,10 +21,14 @@ def cipher(mode: str, string: str, key: int) -> str:
                 new_str += chr((ord(char) - key - 97) % 26 + 97)
             else:
                 new_str += char
+                
     return new_str
 
+"""
+    Main function to run the program.
+"""
 def main():
-    """Main function"""
+    
     mode = input("Type 'encrypt' or 'decrypt': ")
     string = input("Enter a string: ")
     key = int(input("Enter a key (shift number): "))

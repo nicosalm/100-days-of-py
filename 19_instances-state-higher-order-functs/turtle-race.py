2849e925e58cdf67391908a6ab5da5e02b0716d3
Nico Salm <nico@salm.dev>
2023-05-13 20:35:47 -0500

    day 19

---
+++ 19_instances-state-higher-order-functs/turtle-race.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+import random
+
+'''
+    Turtle Race Game
+'''
+
+# screen setup
+screen = Screen()
+screen.setup(width=500, height=400)
+
+# turtle setup
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-100, -60, -20, 20, 60, 100]
+turtles = []
+
+usr_bet = screen.textinput(title="Make your bet!", prompt="Which turtle will win the race? Enter a color (red, orange, yellow, green, blue, purple): ").lower()
+if usr_bet not in colors:
+    raise ValueError("Invalid color entered. Please enter a valid color.")
+
+# create turtles
+for i in range(6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[i])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[i])
+    turtles.append(new_turtle)
+    
+# game loop
+game_on = True
+while game_on:
+    for turtle in turtles:
+        if turtle.xcor() > 230:
+            game_on = False
+            winner = turtle.pencolor()
+            if winner == usr_bet:
+                print(f"You've won! The {winner} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winner} turtle is the winner!")
+        # move turtle a random distance
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+screen.exitonclick()
